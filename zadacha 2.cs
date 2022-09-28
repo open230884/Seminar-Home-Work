4abf5ea91b53dac738a339cb72f4f40df40e1a61
Andrew PV <112729614+open230884@users.noreply.github.com>
2022-09-28 13:31:34 +0300

    написал рабочий код

--- zadacha 2.cs
+++ zadacha 2.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+Console.WriteLine("Введите первое число:");
+
+int n1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+
+int n2 = int.Parse(Console.ReadLine());
+
+if (n1 == n2)
+{
+    Console.WriteLine($"число {n1} равно числу {n2}");
+}
+    else
+        {
+            if (n1 > n2)
+                {
+                    Console.WriteLine($"число {n1} больше числа {n2}");
+                }
+                    else
+                        {
+                            Console.WriteLine($"число {n1} меньше числа {n2}");
+                        }
+        }
